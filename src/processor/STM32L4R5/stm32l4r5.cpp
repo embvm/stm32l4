10a2fd6b84e2908de95cf7b4ccc9e33ede352849
Phillip Johnston <phillip@embeddedartistry.com>
2020-11-02 15:58:43 -0800

    For now, we will define _assert_fail in the processor file as a workaround.

--- src/processor/STM32L4R5/stm32l4r5.cpp
+++ src/processor/STM32L4R5/stm32l4r5.cpp
@@ -2,6 +2,24 @@
 #include <processor_architecture.hpp>
 #include <processor_includes.hpp>
 
+#include <nvic.hpp> // for assert
+
+// TODO: figure out why assert isn't working on arm
+// TODO: Add disableInterrupts() and enableInterrupts() APIs to
+// the base class, and move this implementation to the platform layer.
+void __assert_fail(const char* expr, const char* file,
+
+				   unsigned int line, const char* function)
+{
+	(void)expr;
+	(void)file;
+	(void)line;
+	(void)function;
+	while(1)
+		;
+	NVICControl::disableInterrupts();
+}
+
 #pragma mark - Definitions -
 
 #pragma mark - Helpers -
